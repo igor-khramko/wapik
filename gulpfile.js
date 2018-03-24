5954de46a6b011d8bb15037a6d7fb501f1409ac0
Igor Khramko <igorkhramko@gmail.com>
2018-03-24 14:57:02 +0300

    Last changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const pug = require('gulp-pug');
+const image = require('gulp-image');
+const browserSync = require('browser-sync');
 
 gulp.task('sass', function () {
     return gulp.src('./src/styles/*.scss')
@@ -15,3 +17,27 @@ gulp.task('pug', function () {
   }))
   .pipe(gulp.dest('./build'));
 });
+
+gulp.task('image', function () {
+  gulp.src('./src/images/*')
+    .pipe(image())
+    .pipe(gulp.dest('./build/images'));
+});
+
+gulp.task('browserSync', function () {   
+  browserSync({
+    server: {
+      baseDir: './build/'
+    },
+  })
+});
+
+gulp.task('watch', ['sass', 'pug', 'image', 'browserSync'], function () {   
+  gulp.watch('./src/styles/*.scss', ['sass']);   
+  gulp.watch('./src/pages/*.pug', ['pug']);   
+  gulp.watch('build/*.html', browserSync.reload);   
+  gulp.watch("./build/css/**/*.css").on("change", browserSync.reload);   
+  gulp.watch('./build/js/**/*.js').on("change", browserSync.reload);});
+
+gulp.task('default', ['watch', 'fonts', 'image']);
+  
