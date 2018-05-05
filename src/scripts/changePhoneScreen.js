19d84047da65a215a8841eea2882f4320552975b
i.khramko <igorkhramko@gmail.com>
2018-05-06 02:26:11 +0300

    Final

--- src/scripts/changePhoneScreen.js
+++ src/scripts/changePhoneScreen.js
@@ -1,10 +1,27 @@
-var screen = document.querySelectorAll(".phone__screen");
-for(let i = 0; i < screen.length; i++){
-    screen[i].addEventListener("click", changePhoneScreen);
-}
+var screenItems = document.querySelectorAll(".phone__screen");
+var screen = document.querySelector(".phone");
+screen.addEventListener("click", changePhoneScreen);
+screen.addEventListener("mouseover", altText);
+
 function changePhoneScreen(){
-    for(let i = 0; i < screen.length - 1; i++){
-        screen[i].classList.add("hide-screen");
-        screen[i+1].classList.remove("hide-screen");
-    }  
+    for(let i = 0; i < screenItems.length; i++){
+        if(!screenItems[i].classList.contains("hide-screen")){ 
+            screenItems[i].classList.add("hide-screen"); 
+            if(screenItems[i + 1]){
+                screenItems[i + 1].classList.remove("hide-screen"); 
+            } else{
+                screenItems[0].classList.remove("hide-screen");
+            }
+        return; 
+        } 
+    }
 }
+
+function altText(){
+    for(let i = 0; i < screenItems.length; i++){
+        screenItems[i].setAttribute("title", "Pls, click to change screen");
+    }
+}
+
+
+
